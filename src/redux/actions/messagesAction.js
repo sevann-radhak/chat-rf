cc97895aa09cb49572a0f96ca7a3a77c38c5e5d4
sevannradhak <sevann.radhak@gmail.com>
2019-03-20 18:40:25 -0500

    Prueba commit

--- src/redux/actions/messagesAction.js
+++ src/redux/actions/messagesAction.js
@@ -1,25 +1,28 @@
 import { GET_MESSAGES } from '../actionTypes'
+import { SAVE_NEW_MESSAGES } from '../actionTypes'
 
 // Devuelve los mensajes
 export function getMessages() {
-    console.log("hola");
+    
     return dispatch => {
         window.firebase.database().ref('messages/').on('value', snap => {
 
             dispatch({
                 type: GET_MESSAGES,
                 payload: snap.val()
-            })
-
-            // dispatch({
-            //     type: GET_MESSAGES,
-            //     payload: currentMessages
-            // })            
+            })           
         });
     }
 }
 
 // Almacena un nuevo mensaje
 export function saveNewMessage(newMessage) {
-    return dispatch => window.firebase.database().ref(`messages/${newMessage.id}`).set(newMessage);
+    return dispatch => {
+        window.firebase.database().ref(`messages/${newMessage.id}`).set(newMessage);
+
+        dispatch({
+            type: SAVE_NEW_MESSAGES,
+            payload: this.props
+        })  
+    }
 }
